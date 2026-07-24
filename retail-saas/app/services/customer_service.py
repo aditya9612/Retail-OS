22ac649eaa0a691bcbd7ac8eeea878bee1c1f6b8
rajashri <rajashribhamare1@gmail.com>
2026-07-24 13:31:09 +0530

    customer crm api updates

--- retail-saas/app/services/customer_service.py
+++ retail-saas/app/services/customer_service.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from app.repositories.customer_repo import get_filtered_customers
+from app.repositories.customer_repo import get_filtered_customers ,  get_customer_stats
 
 def fetch_customers(
     db: Session,
@@ -15,5 +15,14 @@ def fetch_customers(
         mobile=mobile,
         segment=segment,
     )
+
+def fetch_customer_stats(
+    db: Session,
+    tenant_id: int,
+):
+    return get_customer_stats(
+        db=db,
+        tenant_id=tenant_id,
+    )    
 
   
