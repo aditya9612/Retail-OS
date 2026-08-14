e2f184395fcd5f55f0fbba1aad7af530b0c6fca9
rajashri <rajashribhamare1@gmail.com>
2026-08-14 11:46:58 +0530

    Fix Employee model import

--- retail-saas/app/models/__init__.py
+++ retail-saas/app/models/__init__.py
@@ -19,6 +19,7 @@ from app.models.role import Role
 from app.models.store import Store
 from app.models.tenant import Tenant
 from app.models.user import User
+from app.models.employee import Employee
 from .coupon import Coupon
 
 __all__ = [
